47926ba19bddc264954ffb498d764bff219f072c
Sandhya-Kanaujiya <sandhya.kanaujia.23espark@gmail.com>
2023-03-18 14:55:18 +0530

    #23DEV071 sidebar design and combine the pages with sidebar and menubar

--- index.js
+++ index.js
@@ -1,40 +1,78 @@
-const express = require('express');
-const mysql = require('mysql2');
+// const express = require('express');
+// const mysql = require('mysql2');
+// const bodyparser = require('body-parser');
+// const app = express();
+// const ejs = require('ejs');
+// const util=require('util');
+// // const bcryptjs = require("bcryptjs");
+// // const jwt=require('jsonwebtoken');
+// // const cookieParser = require('cookie-parser');
+// const { query } = require('express');
+// app.use(express.static("public"));
+
+// //routes
+// const routes = require('../hrms/routes/profile');
+// app.use(routes);
+
+// const routes2 = require('../hrms/routes/dashboard');
+// app.use(routes2);
+
+// const routes3 = require('../hrms/routes/home');
+// app.use(routes3);
+
+
+// app.use(express.json());
+// app.set('view engine', 'ejs');
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: false }));
+
+// app.get('/wizard',(req,res)=>{
+//     res.render('wizard.ejs');
+// });
+
+// app.get('/attendance',(req,res)=>{
+//     res.render('attendance.ejs');
+// });
+
+// app.get('/hotline',(req,res)=>{
+//     res.render('hotline.ejs');
+// });
+
+// app.listen(7070, (err) => {
+//     console.log('server connected at 7070');
+// });
+
+
+
+const express = require('express')
+// const mysql = require('mysql2');
+const expressLayouts = require('express-ejs-layouts')  //Added
 const bodyparser = require('body-parser');
-const app = express();
 const ejs = require('ejs');
-const util=require('util');
+const app = express()
 // const bcryptjs = require("bcryptjs");
 // const jwt=require('jsonwebtoken');
 // const cookieParser = require('cookie-parser');
+const util = require('util');
+const port = 8484
+
+// Static Files
 const { query } = require('express');
-app.use(express.static("public"));
+app.use(express.static('public'))
+// Example for other folders - not required
+// app.use('/css', express.static(__dirname + 'public/css'))
 
-//routes
-const routes = require('../hrms/routes/profile');
-app.use(routes);
-
-const routes2 = require('../hrms/routes/dashboard');
-app.use(routes2);
-
-
+// Set Templating Engine
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.use(expressLayouts)   //Added
+app.set('view engine', 'ejs')
+app.set('layout', './layouts/main') //added
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/wizard',(req,res)=>{
-    res.render('wizard.ejs');
-});
+// Routes
+const routes1 = require('../hrms/routes/routes');
+app.use(routes1);
 
-app.get('/attendance',(req,res)=>{
-    res.render('attendance.ejs');
-});
-
-app.get('/hotline',(req,res)=>{
-    res.render('hotline.ejs');
-});
-
-app.listen(7070, (err) => {
-    console.log('server connected at 7070');
-});
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
---
+++ routes/home.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+// const bodyparser = require('body-parser');
+// const app = express();
+// const ejs = require('ejs');
+// const util=require('util');
+
+// const { query } = require('express');
+// app.use(express.static("public"));
+
+// app.use(express.json());
+// app.set('view engine', 'ejs');
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: false }));
+
+// app.get('/home',(req,res)=>{
+//     res.render('home.ejs');
+// })
+
+// module.exports = app
+
