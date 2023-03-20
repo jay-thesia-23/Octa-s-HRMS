57f5b19fcf258c32237ed414af0d78f374a6329d
omgajipara23 <gajiparaom40@gmail.com>
2023-03-20 19:46:36 +0530

    #23DEV013 register is not working

--- routes/register.js
+++ routes/register.js
@@ -18,15 +18,15 @@ app.use(cookieParser());
 app.use("/public", express.static("public"));
 
 app.use(
-  session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-  maxAge: 1000 * 60 * 60 * 24,
-  },
-  })
-  );
+    session({
+        secret: 'your-secret-key',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+        },
+    })
+);
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -56,55 +56,64 @@ app.post('/clone-email', (req, res) => {
     var email = req.body.email;
     con.query(`select * from registration where u_email='${email}';`, (err, data) => {
         if (err) throw err;
-        res(data);
+        res.json(data);
         // console.log(data.length);
-      }
+    }
     );
-  });
+});
 
 
 app.post("/clone-email", (req, res) => {
-  var email = req.body.email;
-  con.query(
-    `select * from registration where u_email='${email}';`,
-    (err, data) => {
-      if (err) throw err;
-      // console.log(data);
-      if (data.length == 0) {
-        res.json(true);
-      } else {
-        res.json(false);
-      }
-    }
-  );
+    var email = req.body.email;
+    con.query(
+        `select * from registration where u_email='${email}';`,
+        (err, data) => {
+            if (err) throw err;
+            // console.log(data);
+            if (data.length == 0) {
+                res.json(true);
+            } else {
+                res.json(false);
+            }
+        }
+    );
 });
 
 
 app.post("/register", async (req, res) => {
-  var user_name = req.body.name;
-  var email = req.body.email;
-  var password = req.body.password;
+    var user_name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
 
-  req.session.s_email = email
-  console.log(req.session);
+    req.session.s_email = email
+    console.log(req.session);
 
-  var encrypt_password;
-  encrypt_password = await bcrypt.hash(password, 10);
+    var encrypt_password;
+    encrypt_password = await bcrypt.hash(password, 10);
 
-  var sql_insert = `insert into registration (u_name,u_email,u_password,isactive,u_login) values('${user_name}','${email}','${encrypt_password}','1','1');`;
+    var sql_insert = `insert into registration (u_name,u_email,u_password,isactive,u_login) values('${user_name}','${email}','${encrypt_password}','1','1');`;
 
-  con.query(sql_insert, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    }
-  });
+    con.query(sql_insert, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+    });
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+
+        auth: {
+            user: 'hrms1650@gmail.com',
+            pass: 'vymm mlia vhln fuze'
+        }
+    });
 
     const mailConfigurations = {
 
 
-  // const login_token = jwt.sign({ email: email }, "sanjay");
-  // res.cookie("login_token", login_token);
+        // const login_token = jwt.sign({ email: email }, "sanjay");
+        // res.cookie("login_token", login_token);
 
         to: 'sanjayparmar1650@gmail.com',
 
@@ -193,33 +202,33 @@ app.post("/register", async (req, res) => {
         // console.log(info);
     });
 
-  res.send("register Succesfully!!!!");
+    res.send("register Succesfully!!!!");
 });
 
 app.get("/verify", (req, res) => {
-  // const reg_token = req.query.token;
-  const email = req.query.email;
-  
-  if(req.session.s_email == email){
-      res.send("e-mail verification sucesfully!!!!!")
-      con.query(
-        `update registration set isactive = '0' where u_email='${email}';`,
-        (err, data) => {
-          console.log(data);
-        }
-      );
+    // const reg_token = req.query.token;
+    const email = req.query.email;
 
-  }
-  else{
-    res.send('something went wrong!!!!! e-mail is not verfied')
-  }
-  // Verifying the JWT token
-  // jwt.verify(reg_token, "sanjay", function (err, decoded) {
-  //   if (err) {
-  //     console.log(err);
-  //     res.send(
-  //       "Email verification failed possibly the link is invalid or expired"
-  //     );
+    if (req.session.s_email == email) {
+        res.send("e-mail verification sucesfully!!!!!")
+        con.query(
+            `update registration set isactive = '0' where u_email='${email}';`,
+            (err, data) => {
+                console.log(data);
+            }
+        );
+
+    }
+    else {
+        res.send('something went wrong!!!!! e-mail is not verfied')
+    }
+    // Verifying the JWT token
+    // jwt.verify(reg_token, "sanjay", function (err, decoded) {
+    //   if (err) {
+    //     console.log(err);
+    //     res.send(
+    //       "Email verification failed possibly the link is invalid or expired"
+    //     );
     // } else {
     //   console.log(decoded);
     //   res.send("Email verifified successfully");
@@ -230,7 +239,7 @@ app.get("/verify", (req, res) => {
     //     }
     //   );
     // }
-  // });
+    // });
 
 });
 
