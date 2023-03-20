be0d926435c522495a3e67a3c5af22221effd623
Sandhya-Kanaujiya <sandhya.kanaujia.23espark@gmail.com>
2023-03-20 18:48:06 +0530

    #23DEV071 working on hotline backend

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mysql2 = require('mysql2');
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
 app.use(expressLayouts)   //Added
 app.set('layout', './layouts/main') //added
+// var alldataquery = util.promisify(con.query.bind(con));
 
-app.get('/attendance', (req, res) => {
+
+app.get('/attendance', async(req, res) => {
+
+    // var data=await alldataquery(`select * from check_master`);
     res.render('attendance', { title: 'Attendance Page' })
 })
 
