909a3872b4fd0680839dd84b08f21cf894d8c119
omgajipara23 <gajiparaom40@gmail.com>
2023-03-29 09:29:49 +0530

    23DEV0130 changing some functionality

--- public/js/checkinout.js
+++ public/js/checkinout.js
@@ -185,88 +185,11 @@ async function breakout() {
 
 
 
-let clickLeave = 0;
-let clickAttend = 0;
-function myFunction(clickBtn) {
-    if (clickLeave == 0 && clickBtn == 'Leave') {
-        document.getElementById("leave-sub").style.display = "block";
-        clickLeave = 1;
-    }
-    else if (clickLeave == 1 && clickBtn == 'Leave') {
-        document.getElementById("leave-sub").style.display = "none";
-        clickLeave = 0;
-    }
-    if (clickAttend == 0 && clickBtn == 'Attendence') {
-        document.getElementById("attend-sub").style.display = "block";
-        clickAttend = 1;
-    }
-    else if (clickAttend == 1 && clickBtn == 'Attendence') {
-        document.getElementById("attend-sub").style.display = "none";
-        clickAttend = 0;
-    }
-};
-
-function isComment(thisElement) {
-    let commentBtn = document.getElementById('commentBtn');
-    if (((thisElement.value).length) > 0) {
-        commentBtn.style.opacity = 1;
-        commentBtn.style.pointerEvents = 'all';
-    }
-    else {
-        commentBtn.style.opacity = 0.6;
-        commentBtn.style.pointerEvents = 'none';
-    }
-}
-function addComment() {
-    let commentBtn = document.getElementById('commentBtn');
-    let element = document.getElementById('commenter');
-    let commentval = element.value;
-    let comment = '<p style="background-color:#e6ecf5;" >' + commentval + '</p>'
-    let commentBody = document.getElementById('commentBody');
-    commentBody.innerHTML += comment;
-    element.value = "";
-    commentBtn.style.opacity = 0.6;
-    commentBtn.style.pointerEvents = 'none';
-}
-var today = new Date();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-let time = today.toLocaleTimeString();
-element.style.opacity = 0.6;
-element.style.pointerEvents = 'none';
-let breakIn = document.getElementById('breakIn');
-let breakOut = document.getElementById('breakOut');
-let checkOut = document.getElementById('checkOut');
-let attendenceEntry = document.getElementById('attendenceEntry');
-let ThankYou = document.getElementById('ThankYou');
-let entry = '<p style=" color:white; background-color:red;" > Checked Out : ' + time + '</p>';
-breakIn.style.opacity = 0.6;
-breakIn.style.pointerEvents = "none";
-breakOut.style.opacity = 0.6;
-breakOut.style.pointerEvents = "none";
-checkOut.style.opacity = 0.6;
-checkOut.style.pointerEvents = "none"
-attendenceEntry.innerHTML += entry;
-ThankYou.style.display = 'block';
-
-
-let element = document.getElementById('realTime');
-var today = new Date();
-let realTime = formatAMPM(today);
-element.innerHTML = realTime;
-setTimeout(setRealTime, 1000)
 
 
 
 
-function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
-    return strTime;
-}
+
+
+
+
